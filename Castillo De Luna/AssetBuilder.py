6208e8283c180d227eea845262d7265a82f37f02
jtstmu <123123276+jtstmu@users.noreply.github.com>
2023-03-20 21:25:20 -0400

    Update AssetBuilder.py

--- Castillo De Luna/AssetBuilder.py
+++ Castillo De Luna/AssetBuilder.py
@@ -7,10 +7,14 @@ assets="assetText.json"
 characterList = []
 roomsList = []
 itemsList = []
+gameData=[]
+messagesList=[]
 
 # load assets from json file and populates objects 
 with open(assets, 'r') as json_file:
     assetData = json.loads(json_file.read())
+    #for words in assetData['gameData']:
+    #    gameData.append(words)
     for character in assetData['character']['goodCharacter']:
         characterList.append(GoodCharacter(**character))
     for character in assetData['character']['badCharacter']:
@@ -20,7 +24,9 @@ with open(assets, 'r') as json_file:
     for item in assetData['items']['consumables']:
         itemsList.append(Consumable(**item))
     for item in assetData['items']['weapons']:
-        itemsList.append(Weapon(**item))    
+        itemsList.append(Weapon(**item))
+    #for words in assetData['messages']:
+    #    messagesList.append(words)
 
 # characters 
 player = characterList[0]
