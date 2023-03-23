d64fefbf3a328914b52fcef235a2e3f642fa2cfe
michaelkilonzo <michael.kilonzo@torontomu.ca>
2023-03-23 19:29:54 -0400

    wefewfwefwef

--- Castillo De Luna/AssetBuilder.py
+++ Castillo De Luna/AssetBuilder.py
@@ -49,10 +49,3 @@ with open(assets, 'r') as json_file:
     for item in itemsList:
         itemDict[item.getName()] = item
 
-
-player.addInv((itemDict)['SWORD'])
-player.addInv((itemDict)['ELIXIR'])
-player.addInv((itemDict)['ELIXIR'])
-player.addInv((itemDict)['NECKLACE OF DRAGON BEADS'])
-player.addEquip((itemDict)['BACKPACK'])
-print(player.getEquipped())
