0cfbb3cf7c9045849531207d724d46a61a4daf44
Soner Ozturk <soner.ozturk@torontomu.ca>
2023-03-24 20:04:03 -0400

    added type hints and corrected variable and method names

--- Castillo De Luna/Items.py
+++ Castillo De Luna/Items.py
@@ -1,5 +1,5 @@
 class Item:
-    def __init__(self, id, name, firstMessage, secondMessage):
+    def __init__(self, id: int, name: str, firstMessage: str, secondMessage: str):
         self.id = id
         self.name = name
         self.firstMessage = firstMessage
@@ -12,8 +12,8 @@ class Item:
         return f'<Item: {self.name}>'
 
 class Weapon(Item):
-    def __init__(self, id, name, firstMessage, secondMessage, damage):
-        super().__init__(id, name, firstMessage, secondMessage)
+    def __init__(self, id: int, name: str, first_message: str, second_message: str, damage: int):
+        super().__init__(id, name, first_message, second_message)
         self.damage = damage
     
     def isWeapon(self):
@@ -23,9 +23,9 @@ class Weapon(Item):
         return False
 
 class Consumable(Item):
-    def __init__(self, id, name, firstMessage, secondMessage, restoreHealth):
-        super().__init__(id, name, firstMessage, secondMessage)
-        self.restoreHealth = restoreHealth
+    def __init__(self, id: int, name: str, first_message: str, second_message: str, restore_health: int):
+        super().__init__(id, name, first_message, second_message)
+        self.restoreHealth = restore_health
     
     def isWeapon(self):
         return False 
